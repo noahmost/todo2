c4cd08ff54d24df2caac8e5e27de99ba7f95d35c
Noah Mostacero <noahmostacero@gmail.com>
2015-04-24 16:43:07 -0700

    Milestone 4 done

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+if($mysqli->connect_error){
+    die('Connect Error (' . $mysqli->connect_errno . ')'
+            . $mysqli->connect_error);
+}
+else {
+    echo "Connection made";
+}
+$mysqli->close();
+ 
