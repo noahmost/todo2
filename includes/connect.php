97ad0be28268d2dfbbcf33f91658dabbe08b7465
Noah Mostacero <noahmostacero@gmail.com>
2015-04-24 16:49:14 -0700

    5.1 error fixed

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
 if($mysqli->connect_error){
     die('Connect Error (' . $mysqli->connect_errno . ')'
             . $mysqli->connect_error);
